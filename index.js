91e34780e9c9197c2a542f8b639a8e340618c4a1
Joseph-Chege <mjchege23@gmail.com>
2024-03-19 14:23:22 +0300

    Created the functions to modify data in arrays

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Write your solution here!
+
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(Ralph) {
+    cats.push("Ralph");
+        return cats;  
+}
+
+function destructivelyPrependCat(Bob) {
+    cats.unshift("Bob")
+        return cats;
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+        return cats;
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+        return cats;
+}
+
+function appendCat(Broom){
+    const allCats = [...cats, "Broom"];
+        return allCats;
+}
+
+function prependCat(Arnold){
+    const allCats = ["Arnold", ...cats];
+        return allCats;
+}
+
+function removeLastCat() {
+    const removedLastCat = cats.slice(0, cats.length-1);
+        return removedLastCat;
+}
+
+function removeFirstCat() {
+    const removedFirstCat = cats.slice(1);
+        return removedFirstCat;
+}
